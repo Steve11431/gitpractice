888981e3d3949c66bf731de0d2b6c77a2134ec11
Steve11431 <ashpakzari2812@gmail.com>
2025-02-04 15:39:10 +0530

    modified by windows user

---
+++ app.py
@@ -0,0 +1,17 @@
+import random
+import string
+
+def generate_password(length=12, use_digits=True, use_specials=True):
+    characters = string.ascii_letters
+    if use_digits:
+        characters += string.digits
+    if use_specials:
+        characters += string.punctuation
+    
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+
+# Example usage
+password = generate_password(16)
+print("Generated Password:", password)
+
